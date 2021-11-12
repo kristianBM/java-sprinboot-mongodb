b6ace6b834edf88a8b9a7cf28c4a1e9520071135
Kristian Mello <kristian.bonfim@hotmail.com>
2021-11-12 06:39:42 -0300

    First commit, project created

---
+++ src/test/java/com/surfersolution/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surfersolution.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
