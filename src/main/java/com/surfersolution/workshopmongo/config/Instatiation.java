e7a492c2a75f9700d02ec1f6da34f236fe3c9791
Kristian Mello <kristian.bonfim@hotmail.com>
2021-11-15 16:20:56 -0300

    adding comments to posts

--- src/main/java/com/surfersolution/workshopmongo/config/Instatiation.java
+++ src/main/java/com/surfersolution/workshopmongo/config/Instatiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.surfersolution.workshopmongo.domain.Post;
 import com.surfersolution.workshopmongo.domain.User;
 import com.surfersolution.workshopmongo.dto.AuthorDTO;
+import com.surfersolution.workshopmongo.dto.CommentDTO;
 import com.surfersolution.workshopmongo.repositories.PostRepository;
 import com.surfersolution.workshopmongo.repositories.UserRepository;
 
@@ -42,6 +43,12 @@ public class Instatiation implements CommandLineRunner{
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "partiu viagem", "Vou viajar para São Paulo. Abraços", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia.", "Hoje eu acordei feliz", new AuthorDTO(maria));
 		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("22/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha uma ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
